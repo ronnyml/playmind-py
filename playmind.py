58e74e5230cdca166b0cff18e66709ebdf3156a0
Ronny Yabar <ronnycontacto@gmail.com>
2024-12-22 22:58:50 -0500

    refactoring to improve readability and remove redundant logic.

--- playmind.py
+++ playmind.py
@@ -31,8 +31,15 @@ LEVELS = [
     'Difficult'
 ]
 
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
 
-class PlayMindGame(object):
+
+class PlayMindGame:
     """Base class definition of the game."""
 
     def __init__(self, operation, level, num_questions):
@@ -45,15 +52,8 @@ class PlayMindGame(object):
 
     @staticmethod
     def get_result(operation, num_a, num_b):
-        """Round the result of the operation between two numbers."""
-        result = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-            '/': operator.truediv,
-        }[operation](num_a, num_b)
-
-        return round(result, 1)
+        """Perform the operation and round the result."""
+        return round(OPERATORS[operation](num_a, num_b), 1)
 
     def init_game(self):
         """Ask the user if ready to start the game."""
@@ -66,19 +66,15 @@ class PlayMindGame(object):
             print('Choose y or n')
 
     def start_game(self):
-        """Start the timer and show the operations to calculate one by one."""
+        """Run the game by generating questions and recording scores."""
         start_time = time.perf_counter()
         operations = ['+', '-', '*', '/']
-        for i in range(self.num_questions):
-            num_a = self.get_random_num()
-            num_b = self.get_random_num()
-            if num_a < num_b:
-                num_a, num_b = num_b, num_a
-            operator_str = operations[self.operation - 1]
+        operator_str = operations[self.operation - 1]
+
+        for i in range(1, self.num_questions + 1):
+            num_a, num_b = sorted([self.get_random_num(), self.get_random_num()], reverse=True)
             result = self.get_result(operator_str, num_a, num_b)
-            question = ' {0}/{1}:  {2} {3} {4} = '.format(
-                i + 1, self.num_questions, num_a, operator_str, num_b
-            )
+            question = f' {i}/{self.num_questions}:  {num_a} {operator_str} {num_b} = '
             user_answer = get_positive_int(question)
 
             if result == user_answer:
@@ -86,18 +82,16 @@ class PlayMindGame(object):
             else:
                 self.num_incorrect += 1
 
-        end_time = time.perf_counter()
-        self.total_time = round(end_time - start_time, 2)
+        self.total_time = round(time.perf_counter() - start_time, 2)
         self.show_highscores()
 
     def get_random_num(self):
-        """Get a random integer number."""
-        min_num = self.get_level_nums()[0]
-        max_num = self.get_level_nums()[1]
+        """Generate a random number within the level range."""
+        min_num, max_num = self.get_level_nums()
         return randint(min_num, max_num)
 
     def get_level_nums(self):
-        """Generate the limit numbers based on the level.
+        """Generate the range of numbers based on the level..
 
         Get a list with the minimun and maximum to generate random numbers.
             [10, 99]
@@ -108,44 +102,24 @@ class PlayMindGame(object):
             ...
 
         """
-        start = 1
-        limits = []
-        for _ in range(self.level):
-            start *= 10
-        end = start*10 - 1
-
-        limits.append(start)
-        limits.append(end)
-        return limits
+        start = 10 ** (self.level - 1)
+        end = 10 ** self.level - 1
+        return start, end
 
     def show_highscores(self):
-        """Show highscores like the time and number of correct answers."""
+        """Display highscores in a formatted table."""
         print('\n\t\tHIGHSCORES')
-        separators = ' {} | {} | {} | {} | {} | {}'
-        headers = [
-            'Operation', 'Level', 'Correct',
-            'Incorrect', 'Total', 'Time'
-        ]
+        headers = ['Operation', 'Level', 'Correct', 'Incorrect', 'Total', 'Time']
         highscores = [
             OPERATIONS[self.operation - 1], LEVELS[self.level - 1],
             self.num_correct, self.num_incorrect,
             self.num_questions, self.total_time
         ]
 
-        for i, _ in enumerate(highscores):
-            len_header = len(headers[i])
-            len_highscore = len(str(highscores[i]))
-            if len_header < len_highscore:
-                num_spaces = len_highscore - len_header
-                spaces = ' ' * num_spaces
-                headers[i] = headers[i] + spaces
-            else:
-                num_spaces = len_header - len_highscore
-                spaces = ' ' * num_spaces
-                highscores[i] = str(highscores[i]) + spaces
-
-        print(separators.format(*headers))
-        print(separators.replace('|', ' ').format(*highscores))
+        # Print header row
+        print(" | ".join(header.ljust(12) for header in headers))
+        # Print scores
+        print(" | ".join(str(score).ljust(12) for score in highscores))
         self.check_win()
 
     def check_win(self):
@@ -157,17 +131,15 @@ class PlayMindGame(object):
 
 
 def get_game_option(message, options):
-    """Show a list of options based on the game question."""
+    """Prompt user to select an option from a list."""
     while True:
-        print('{}: '.format(message))
-        for i, option in enumerate(options):
-            print(' {} {}'.format(i + 1, option))
-
-        option = get_positive_int(' : ')
-        if option <= len(options):
-            break
-
-    return option
+        print(f'{message}:')
+        for i, option in enumerate(options, 1):
+            print(f' {i}. {option}')
+        choice = get_positive_int('Choose an option: ')
+        if 1 <= choice <= len(options):
+            return choice
+        print('Invalid choice. Please select a valid option.')
 
 
 def get_positive_int(option):
